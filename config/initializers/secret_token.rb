bd282114619b520160e0b1b6b6c7ee244ac5777e
Vu Pham <vupham0326@mail.com>
2015-06-04 07:39:44 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tastea::Application.config.secret_key_base = '94a98213da5a9059469b5913dae4a835bb8d13a6ae8168cf66f0f9b0e8a5fb8000f4931547355efea635726d511d182df81293a83b4ac1a105cdcfb71626987f'
